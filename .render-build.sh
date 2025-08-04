6c83ac5136fbceef986819a17e62db4f65b467f0
Avikumar1234 <111271460+Avikumar1234@users.noreply.github.com>
2025-08-04 09:11:53 -0700

    Update .render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-set -o errexit
 
-composer install --no-dev
-php artisan config:cache
+# Install PHP dependencies
+composer install --no-interaction --prefer-dist --optimize-autoloader
+
+# Install Node dependencies and build assets
 npm install
 npm run build
-php artisan migrate --force
+
+# Laravel config and key
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
