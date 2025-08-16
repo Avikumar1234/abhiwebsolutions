309191cdea9401cfc20f1e195cedae506fd11cae
Avikumar1234 <111271460+Avikumar1234@users.noreply.github.com>
2025-08-16 12:07:52 -0700

    Update home.blade.php

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -129,12 +129,7 @@
         </div>
     </div>
 
-<!--     <div class="text-center mt-8">
-        <a href="{{ url('/avi') }}"
-        class="inline-block bg-blue-600 text-white px-6 py-3 rounded-full shadow-md hover:bg-blue-700 transition duration-300">
-            Available Services
-        </a>
-    </div> -->
+
 </section>
 
 <!-- 🔹 Projects -->
